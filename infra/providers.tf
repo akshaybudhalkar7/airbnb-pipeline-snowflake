d92e6dc35ae3f3f73b7bc3e31ba21283bad694c5
unknown <akshaybudhalkar7@gmail.com>
2026-08-04 11:57:44 +0530

    udpated providers file

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,15 +1,15 @@
 terraform {
-  required_version = ">= 2.13"
+  required_version = ">= 1.11"
 
   required_providers {
     snowflake = {
       source  = "snowflakedb/snowflake"
-      version = "~> 1.0"
+      version = "~> 2.13"
     }
   }
 
   backend "s3" {
-    bucket       = "airbnb-pipeline-tf-656559336744-us-east-1 "
+    bucket       = "airbnb-pipeline-tf-656559336744-us-east-1"
     key          = "snowflake/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true

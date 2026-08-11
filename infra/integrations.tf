254f4f2f5b191cb6217003f2d0d180573c6096eb
unknown <akshaybudhalkar7@gmail.com>
2026-08-11 09:35:13 +0530

    creating storage integration

--- infra/integrations.tf
+++ infra/integrations.tf
@@ -3,6 +3,6 @@ resource "snowflake_storage_integration_aws" "s3_raw" {
   storage_provider          = "S3"
   enabled                   = "true"
   storage_aws_role_arn      = "arn:aws:iam::656559336744:role/airbnb-pipeline-snowflake-s3-access"
-  storage_allowed_locations = ["airbnb-pipeline-data-lake-656559336744-us-east-1/"]
+  storage_allowed_locations = ["s3://airbnb-pipeline-data-lake-656559336744-us-east-1/"]
   comment                   = "Managed by Terraform"
 }
